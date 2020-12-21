45291a49c2ac835e29d332bb53b0776c072dd2be
cthakar <thakarhchintan@gmail.com>
2020-12-21 17:41:14 +0530

    Fix related to sh from bash

--- pipeline-creator.sh
+++ pipeline-creator.sh
@@ -6,8 +6,8 @@ echo "--------------------Creating codefresh pipelines-----------------------"
 #declare -a new
 #declare -a final
 
-existing=(`codefresh get pipelines | awk '{print $1}' | grep -v "NAME" | grep "dynamic-pipelines" | cut -d '/' -f 2`)
-new=(`ls ./projects/services/`)
+existing=`codefresh get pipelines | awk '{print $1}' | grep -v "NAME" | grep "dynamic-pipelines" | cut -d '/' -f 2`
+new=`ls ./projects/services/`
 final=$(echo ${new[@]} ${existing[@]} | sed 's/ /\n/g' | sort | uniq -d | xargs echo ${new[@]} | sed 's/ /\n/g' | sort | uniq -u)
 
 
