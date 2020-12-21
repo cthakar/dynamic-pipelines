d4a3a78d9e519289c6dad368418127f871df276f
cthakar <thakarhchintan@gmail.com>
2020-12-21 17:49:29 +0530

    fixes to sh errors

--- pipeline-creator.sh
+++ pipeline-creator.sh
@@ -6,14 +6,15 @@ echo "--------------------Creating codefresh pipelines-----------------------"
 #declare -a new
 #declare -a final
 
-existing=`codefresh get pipelines | awk '{print $1}' | grep -v "NAME" | grep "dynamic-pipelines" | cut -d '/' -f 2`
-new=`ls ./projects/services/`
-final=$(echo ${new[@]} ${existing[@]} | sed 's/ /\n/g' | sort | uniq -d | xargs echo ${new[@]} | sed 's/ /\n/g' | sort | uniq -u)
 
+
+set -A existing=(`codefresh get pipelines | awk '{print $1}' | grep -v "NAME" | grep "dynamic-pipelines" | cut -d '/' -f 2`)
+set -A new=(`ls ./projects/services/`)
+set -A final=$(echo ${new[@]} ${existing[@]} | sed 's/ /\n/g' | sort | uniq -d | xargs echo ${new[@]} | sed 's/ /\n/g' | sort | uniq -u)
 
 
 for svc in $final
 do
-    #echo "codefresh create pipeline -f ./projects/services/$svc/pipeline-spec.yml"
+    echo "codefresh create pipeline -f ./projects/services/$svc/pipeline-spec.yml"
     codefresh create pipeline -f ./projects/services/$svc/pipeline-spec.yml
 done
